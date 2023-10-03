1453ff7408ebe3b632922598b6b533c6f8c5f853
Dulon <dulonmolla88@gmail.com>
2023-10-03 10:44:22 +0600

    nasted route

---
+++ app/about/page.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div>
+      <p className='text-4xl'>About page</p>
+    </div>
+  );
+}
---
+++ app/contact/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Contact = () => {
+  return <div className='text-4xl'>Contact page</div>;
+};
+
+export default Contact;
